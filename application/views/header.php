a9557c291f0fa21afcb9f8c872c6791503773d2b
tomual <tomual@users.noreply.github.com>
2018-10-29 18:39:33 +0800

    HTML Boilerplate, Bootstrap, page template

---
+++ application/views/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Site Title</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="<?php echo base_url('css/normalize.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/main.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/style.css') ?>">
+</head>
+
+<body>
---
+++ application/views/home.php
@@ -0,0 +1,101 @@
+<?php $this->load->view('header') ?>
+
+<header>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h1>Site Name</h1>
+				<div class="subtitle">Website Development Tools</div>
+			</div>
+		</div>
+	</div>
+</header>
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<a href="#" class="badge badge-primary">Primary</a>
+			<a href="#" class="badge badge-secondary">Secondary</a>
+			<a href="#" class="badge badge-success">Success</a>
+			<a href="#" class="badge badge-danger">Danger</a>
+			<a href="#" class="badge badge-warning">Warning</a>
+			<a href="#" class="badge badge-info">Info</a>
+			<a href="#" class="badge badge-light">Light</a>
+			<a href="#" class="badge badge-dark">Dark</a>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-2">
+			<div class="card">
+				<img class="card-img-top" src="https://placeholdit.co/i/500x300" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-2">
+			<div class="card">
+				<img class="card-img-top" src="https://placeholdit.co/i/500x300" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-2">
+			<div class="card">
+				<img class="card-img-top" src="https://placeholdit.co/i/500x300" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-2">
+			<div class="card">
+				<img class="card-img-top" src="https://placeholdit.co/i/500x300" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-2">
+			<div class="card">
+				<img class="card-img-top" src="https://placeholdit.co/i/500x300" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-2">
+			<div class="card">
+				<img class="card-img-top" src="https://placeholdit.co/i/500x300" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				Footer
+			</div>
+		</div>
+	</div>
+</footer>
+
+<?php $this->load->view('footer') ?>
