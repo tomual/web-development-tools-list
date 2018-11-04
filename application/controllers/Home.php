ccf40095056c4ec52794ae27fc16cc1dedc8f9bd
tomual <tomual@users.noreply.github.com>
2018-11-04 21:48:23 +0800

    Confirm data file exists

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,9 +5,12 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$json = file_get_contents("data/src.json");
-		$tools = json_decode($json);
-		$tools = $tools->tools;
+		$tools = array();
+		if (file_exists("data/src.json")) {
+			$json = file_get_contents("data/src.json");
+			$tools = json_decode($json);
+			$tools = $tools->tools;
+		}
 		$this->load->view('home', compact('tools'));
 	}
 }
--- application/controllers/Tag.php
+++ application/controllers/Tag.php
@@ -5,9 +5,15 @@ class Tag extends CI_Controller {
 
 	public function _remap($tag)
 	{
-        $json = file_get_contents("data/$tag.json");
-		$tools = json_decode($json);
-		$tools = $tools->tools;
-		$this->load->view('tag', compact('tools', 'tag'));
+		if (file_exists("data/$tag.json")) {
+			$json = file_get_contents("data/$tag.json");
+			$tools = json_decode($json);
+			if (!empty($tools->tools)) {
+				$tools = $tools->tools;
+				$this->load->view('tag', compact('tools', 'tag'));
+				return;
+			}
+		}
+		show_404();
 	}
 }
